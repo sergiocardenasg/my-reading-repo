b77ac4bb0afcfcd86afd0a55e452b1cf06ec41f6
sergiocardenasg <sergiocardenas@utexas.edu>
2020-11-06 18:50:48 -0600

    began login/signup pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,49 @@
 class UsersController < ApplicationController
+    
+    get '/signup' do
+        if logged_in?
+            redirect to '/lists'
+        else
+            erb :'users/signup'
+        end
+    end
 
+    post '/signup' do
+        if params[:email] == "" || params[:password] == ""
+            redirect '/signup'
+        else
+            user = User.new(params)
+            user.save
+            session[:user_id] = user.id
+            redirect '/lists'
+        end
+    end
+
+    get '/login' do
+        if logged_in?
+            redirect '/lists'
+        else
+            erb :'users/login'
+        end
+    end
+
+	post '/login' do
+		user = User.find_by(:email => params[:email])
+        if user && user.authenticate(params[:password])
+            session[:user_id] = user.id
+            redirect '/lists'
+        else
+			redirect '/login'
+		end
+    end
+    
+    get '/logout' do
+        if logged_in?
+          session.destroy
+          redirect '/login'
+        else
+          redirect '/'
+        end
+    end
 
 end
