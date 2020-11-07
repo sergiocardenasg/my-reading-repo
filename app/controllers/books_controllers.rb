48b6338cce677ad4b63a4f54d8cf888db8064245
sergiocardenasg <sergiocardenas@utexas.edu>
2020-11-07 11:23:18 -0600

    started create actions for persisting data in DB

--- app/controllers/books_controllers.rb
+++ app/controllers/books_controllers.rb
@@ -8,4 +8,28 @@ class BooksController < ApplicationController
         end
     end
 
+    get '/books/new' do
+        if logged_in?
+            erb :'books/create'
+        else
+            redirect '/login'
+        end
+    end
+
+    # post '/books' do
+    #     if logged_in?
+    #         if params[:content] == ""
+    #             redirect '/tweets/new'
+    #         else
+    #             @tweet = Tweet.new(params)
+    #             if @tweet.save
+    #                 current_user.tweets << @tweet
+    #                 redirect "/tweets/#{@tweet.id}"
+    #             end
+    #         end
+    #     else
+    #         redirect to "/tweets/new"
+    #     end
+    # end
+
 end
